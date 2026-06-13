dcad1ddf60dbbc24627155fd49279708d39c7342
djukic03 <djukic.nemanja003@gmail.com>
2026-06-13 23:45:39 +0200

    Prefab kartica

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName ="New Card", menuName ="Card")]
+public class Card : ScriptableObject
+{
+    public Image cardImage;
+    public string cardName;
+    public CardType cardType; //att, def...
+    public string effectDescription;
+    public int nitroPoints; //koliko kosta da se aktivira
+    public int damage; //koliko damage-a dilujemo protivnikovom fuel-u
+    public int nitroBuff; // ZA (DE)BUFF koliko dodaje/oduzima nitra
+    public int fuelBuff; // ZA (DE)BUFF koliko dodaje/oduzima fuel-a
+    public List<TargetLane> targetLanes; // koje lejnove pogadja
+}
